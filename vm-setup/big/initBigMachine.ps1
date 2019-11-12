fc8f49db5d060e0d1ba6fa9c06af0dfc51f5171f
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-11-12 14:02:43 +0100

    init

---
+++ vm-setup/big/initBigMachine.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install git
+choco install vscode
+choco install microsoft-edge-insider
+choco install googlechrome
+choco install firefox
+
+cd "$([Environment]::GetFolderPath("Desktop"))"
+wget https://github.com/tfenster/presentation-src/archive/techdays-19.zip -OutFile sources.zip
+Expand-Archive .\sources.zip
+rm .\sources.zip
---
+++ vm-setup/small/initSmallMachine.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install microsoft-edge-insider
+choco install googlechrome
+choco install firefox
+choco install docker-cli
