7f9a9456bf12bbfb7af71b34814e13717bd03689
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-11-13 15:48:50 +0100

    add mail sending

--- vm-setup/deploy.ps1
+++ vm-setup/deploy.ps1
@@ -38,8 +38,24 @@ param(
  $name,
 
  [Parameter(Mandatory=$True)]
- [securestring]
- $adminPassword,
+ [string]
+ $adminPasswordPlain,
+
+ [Parameter(Mandatory=$True)]
+ [pscredential]
+ $mailCredential,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $mailFrom,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $mailTo,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $personName,
 
  [Parameter(Mandatory=$False)]
  [switch]
@@ -107,6 +123,8 @@ Function ConvertPSObjectToHashtable
 #******************************************************************************
 $ErrorActionPreference = "Stop"
 
+$adminPassword = (ConvertTo-SecureString -String $adminPasswordPlain -AsPlainText -Force)
+
 if ($initial) {
     # sign in
     Write-Host "Logging in...";
@@ -141,3 +159,24 @@ Write-Host "Starting big deployment...";
 New-AzResourceGroupDeployment -ResourceGroupName $name -Name "$name-big-deployment" -TemplateFile (Join-Path $PSScriptRoot "big\template.json") -TemplateParameterFile (Join-Path $PSScriptRoot "\big\parameters.json") -location $location -adminPassword $adminPassword -dnsLabelPrefix "td19-$name-big" -AsJob
 Write-Host "Starting small deployment...";
 New-AzResourceGroupDeployment -ResourceGroupName $name -Name "$name-small-deployment" -TemplateFile (Join-Path $PSScriptRoot "small\template.json") -TemplateParameterFile (Join-Path $PSScriptRoot "small\parameters.json") -location $location -adminPassword $adminPassword -dnsLabelPrefix "td19-$name-small" -AsJob
+
+# Send mail
+$smtpServer = "smtp.office365.com"
+$smtpPort = "587"
+$body = "Hello $personName,<br />&nbsp;<br/>"
+$body += "welcome to the `"Docker on Windows 101 and Business Central on Docker`" workshop at NAV TechDays! Please use the following connection information to access your VMs through a Remote Desktop connection:<br />&nbsp;<br/>"
+$body += "Big VM (host): computer name td19-$name-big.$location.cloudapp.azure.com, user name \TechDaysAdmin, password $adminPasswordPlain<br/>"
+$body += "Small VM (host): computer name td19-$name-small.$location.cloudapp.azure.com, user name \TechDaysAdmin, password $adminPasswordPlain<br/>&nbsp;<br />"
+$body += "Please note that it might take up to 20 minutes until the VMs are available<br/>&nbsp;<br />"
+$body += "Have fun,<br />Tobias"
+$mailParam = @{
+    To = $mailTo
+    From = $mailFrom
+    Subject = "Access information for your NAV TechDays workshop VM"
+    Body = $body
+    SmtpServer = $smtpServer
+    Port = $smtpPort
+    Credential = $mailCredential
+}
+
+Send-MailMessage @mailParam -UseSsl -BodyAsHtml
