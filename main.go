1a212dd1dcf5537e67639cdba0fafd366884614d
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2023-06-21 07:52:39 +0000

    initial

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	_ "image/jpeg"
+	_ "image/png"
+	"syscall/js"
+
+	"bctechdays/image-converter/convert"
+)
+
+func converter(this js.Value, inputs []js.Value) interface{} {
+	array := inputs[0]
+	inBuf := make([]uint8, array.Get("byteLength").Int())
+	js.CopyBytesToGo(inBuf, array)
+	convertOptions := convert.Options{}
+	err := json.Unmarshal([]byte(inputs[1].String()), &convertOptions)
+	if err != nil {
+		convertOptions = convert.DefaultOptions
+	}
+
+	converter := convert.NewImageConverter()
+	return converter.ImageFile2ASCIIString(inBuf, &convertOptions)
+}
+
+func main() {
+	c := make(chan bool)
+	js.Global().Set("convert", js.FuncOf(converter))
+	<-c
+}
